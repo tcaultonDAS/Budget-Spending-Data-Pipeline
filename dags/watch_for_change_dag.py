0cf240735a902bfd4254d16e4a7ac255fe33d472
Teddy Caulton <t.caulton@das42.com>
2022-08-26 08:38:27 -0600

    uploading dag that needs to be manually triggered

--- dags/watch_for_change_dag.py
+++ dags/watch_for_change_dag.py
@@ -10,7 +10,10 @@ spend_sheet = Sheet2APIClient(api_url = 'https://sheet2api.com/v1/lSkMlXOdT5aS/d
 ted_budget = Sheet2APIClient(api_url = 'https://sheet2api.com/v1/lSkMlXOdT5aS/teddy_budget_2022')
 
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+months_spend = ['July', 'August', 'September', 'October', 'November', 'December']
 
+grocery_data = {'January':0, 'February':0, 'March':0, 'April':0, 'May':0, 'June':0, 'July':0, 'August':0, 'September':0, 'October':0, 'November':0, 'December':0}
+utility_data = {'January':0, 'February':0, 'March':0, 'April':0, 'May':0, 'June':0, 'July':0, 'August':0, 'September':0, 'October':0, 'November':0, 'December':0}
 
 default_args = {
 	'start_date': datetime(2020, 1, 1)
@@ -18,56 +21,72 @@ default_args = {
 
 def _get_data_groc(**kwargs):
 	ti = kwargs['ti']
-	data_groc = spend_sheet.get_rows(sheet = 'August', query={'Category':'Groceries'})
 
-	prices = []
-	if len(data_groc) == 1:
-		Grocery_Sum = data_groc['Amount']
-	elif len(data_groc) > 1:
-		for d in data_groc:
-			prices.append(d['Amount'])
-		Grocery_Sum = sum(prices)/3
-	else:
-		Grocery_Sum = 0
+	for i in months_spend:
+		data_groc = spend_sheet.get_rows(sheet = i, query={'Category':'Groceries'})
 
-	ti.xcom_push(key = 'groceries_pull', value = Grocery_Sum)
+		prices = []
+		if len(data_groc) == 1:
+			Grocery_Sum = data_groc['Amount']
+			grocery_data[i] = Grocery_Sum
+		elif len(data_groc) > 1:
+			for d in data_groc:
+				prices.append(d['Amount'])
+			Grocery_Sum = sum(prices)/3
+			grocery_data[i] = Grocery_Sum
+		else:
+			Grocery_Sum = 0
+			grocery_data[i] = Grocery_Sum
+
+	ti.xcom_push(key = 'groceries_pull', value = grocery_data)
+	print(grocery_data['July'])
 
 def _get_data_util(**kwargs):
 	ti = kwargs['ti']
-	data_util = spend_sheet.get_rows(sheet = 'August', query={'Category':'Utilities'})
 
-	prices = []
-	if len(data_util) == 1:
-		Utilities_Sum = data_util[0]['Amount']/3
-	elif len(data_util) > 1:
-		for d in data_util:
-			prices.append(d['Amount'])
-		Utilities_Sum = sum(prices)/3
-	else:
-		Utilities_Sum = 0
+	for i in months_spend:
+		data_util = spend_sheet.get_rows(sheet = i, query={'Category':'Utilities'})
 
-	ti.xcom_push(key = 'utilities_pull', value = Utilities_Sum)
+		prices = []
+		if len(data_util) == 1:
+			Utilities_Sum = data_util[0]['Amount']/3
+			utility_data[i] = Utilities_Sum
+		elif len(data_util) > 1:
+			for d in data_util:
+				prices.append(d['Amount'])
+			Utilities_Sum = sum(prices)/3
+			utility_data[i] = Utilities_Sum
+		else:
+			Utilities_Sum = 0
+			utility_data[i] = Utilities_Sum
+
+	ti.xcom_push(key = 'utilities_pull', value = utility_data)
 
 
 def _update_budget(**kwargs):
 	ti = kwargs['ti']
-	Grocery_Sum = ti.xcom_pull(key = 'groceries_pull', task_ids = ['get_data_groc'])
-	Grocery_Sum = int(Grocery_Sum[0])
-	Utilities_Sum = ti.xcom_pull(key = 'utilities_pull', task_ids = ['get_data_util'])
-	Utilities_Sum = int(Utilities_Sum[0])
 
-	print(Grocery_Sum)
-	print(Utilities_Sum)
+	grocery_data = ti.xcom_pull(key = 'groceries_pull', task_ids = ['get_data_groc'])
+	utility_data = ti.xcom_pull(key = 'utilities_pull', task_ids = ['get_data_util'])
+	print(grocery_data)
+	print(utility_data)
 
-	ted_budget.update_rows(
-		sheet = 'Data_Load',
-		query = {'month': 'August'},
-		row = {
-			'groceries': Grocery_Sum,
-			'utilities': Utilities_Sum
-		},
-		partial_update = True,
-	)
+	for i in months:
+		Grocery_Sum = grocery_data[0][i]
+		Grocery_Sum = int(Grocery_Sum)
+
+		Utilities_Sum = utility_data[0][i]
+		Utilities_Sum = int(Utilities_Sum)
+
+		ted_budget.update_rows(
+			sheet = 'Data_Load',
+			query = {'month': i},
+			row = {
+				'groceries': Grocery_Sum,
+				'utilities': Utilities_Sum
+			},
+			partial_update = True,
+		)
 
 
 
